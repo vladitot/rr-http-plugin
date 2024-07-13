4260fde7c2ba13ed8a6efc2cf24f24b231cc0d7e
vladimir zakotnev <vladitot@gmail.com>
2024-07-13 18:59:22 +0200

    build preparation

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 test:
 	go test -v -race -cover -tags=debug ./...
+
+proto:
+	mkdir -p proto_objects
+	podman run --rm -v $(shell pwd):/app -w /app rvolosatovs/protoc --go_out=/app/proto_objects --proto_path=/app/protofiles /app/protofiles/release.proto
---
+++ rpc.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	protofiles_v1 "github.com/roadrunner-server/http/v5/proto_objects/protofiles.v1"
+	"go.uber.org/zap"
+)
+
+type rpc struct {
+	srv *Plugin
+	log *zap.Logger
+}
+
+func (rpc *rpc) Release(request *protofiles_v1.ReleaseRequestV1, response *protofiles_v1.ReleaseResponseV1) error {
+
+	pid := request.Pid
+	err := rpc.srv.pool.Release(pid)
+	if err != nil {
+		response.Ok = 2
+		return nil
+	}
+	response.Ok = 1
+	return nil
+
+}
